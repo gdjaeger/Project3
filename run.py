8e7ffe6fc0b1008d72ab8a57d02e72378eb1ed22
gdjaeger <grahamjaeger@Grahams-MacBook-Pro.local>
2023-09-06 20:35:05 -0500

    updated

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 from app import app
+import os
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use the PORT environment variable if available, otherwise, use port 5000 as a default
+    port = int(os.environ.get("PORT", 5000))
+    
+    app.run(host='0.0.0.0', port=port, debug=True)
