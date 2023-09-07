a5bb79774204f68c8233cd9731b4445a8f275895
gdjaeger <grahamjaeger@Grahams-MacBook-Pro.local>
2023-09-06 20:45:54 -0500

    final

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 import os
 
+port = int(os.environ.get("PORT", 5000))  # Use port 5000 as a default if not set
+
 app = Flask(__name__)
 app.static_folder = 'static'
 
-# Use the PORT environment variable if it's available, otherwise, use port 5000 as a default
-port = int(os.environ.get("PORT", 5000))
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
+from app import routes
